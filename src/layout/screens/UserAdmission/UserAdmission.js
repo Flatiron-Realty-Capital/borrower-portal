ae5f6a8af3337eb60767907aa02f20304b62edd0
jo-flatiron <jo@flatironrealtycapital.com>
2022-06-07 18:07:26 -0400

    Added Portal after login

---
+++ src/layout/screens/UserAdmission/UserAdmission.js
@@ -0,0 +1,22 @@
+import React from "react";
+import BackgroundShape from "./components/backgroundShape/backgroundShape";
+import SignInForm from "./components/form/form";
+import Stats from "./components/stats/stats";
+
+const UserAdmission = (props) => {
+  return (
+    <>
+      <BackgroundShape />
+      <div className="main-inner-wrapper">
+        <section id="form">
+          <SignInForm />
+        </section>
+        <section id="stats">
+          <Stats />
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default UserAdmission;
